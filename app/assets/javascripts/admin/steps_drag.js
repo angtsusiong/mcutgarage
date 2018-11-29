34fb56eed36036f416a3e003c3d8a3732da5c3d1
AngTsuiong <angtsusiong@gmail.com>
2018-11-29 05:17:37 +0000

    d

--- app/assets/javascripts/admin/steps_drag.js
+++ app/assets/javascripts/admin/steps_drag.js
@@ -12,18 +12,6 @@
                 order.value = i;
                 i += 2;
             });
-            var new_pri = document.querySelectorAll('div.drag-item-priority');
-            Array.from(new_pri).forEach(function(el){
-                var new_order = el.querySelector('.integer');
-                $.ajax({
-                    url: "./update_drag",
-                    type: "PATCH",
-                    datatype: "json",
-                    data: {
-                        id: 2,
-                        priority: new_order.value }
-                });
-            });
 
         });
 })();
